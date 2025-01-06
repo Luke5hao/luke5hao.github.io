52b7a8c603db8aa5a86f17989bbd8a0dc4dceb2e
Luke Shao <lys8@cornell.edu>
2025-01-06 16:38:10 -0500

    Update skills and experience

---
+++ scripts/skills.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const skillsList = [
+  { name: "Python" },
+  { name: "Java" },
+  { name: "HTML" },
+  { name: "CSS" },
+  { name: "JavaScript" },
+  { name: "React" },
+  { name: "C" },
+  { name: "SQL" },
+  { name: "pandas" },
+  { name: "NumPy" },
+  { name: "Docker" },
+  { name: "Github" },
+  { name: "VS Code" },
+  { name: "Jupyter" },
+  { name: "MS Excel" },
+  { name: "Figma" },
+];
+
+// FROM RESUME
+// Programming Languages: Python, Java, C, SQL, HTML, CSS, JavaScript, OCaml
+// Frameworks & Tools: pandas, NumPy, PyTorch, MySQL, React, Docker, Gym, Github, Visual Studio Code, Jupyter
+// Other: Microsoft Office (Excel, Powerpoint, Word), Google Workspace, Tableau, Figma, Canva
+
+const skillsContainer = document.getElementById("skills-container");
+skillsList.forEach((skill) => {
+  // Create the skill element and append to container
+  const element = document.createElement("div");
+  element.textContent = skill.name; 
+
+  skillsContainer.appendChild(element);
+});
